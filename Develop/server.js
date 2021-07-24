2416fe8cad8e9391b00e7fde8a7b488f4b3552cd
djkaz <justbekazu@gmail.com>
2021-07-23 20:57:46 -0500

    working on server connection

--- Develop/server.js
+++ Develop/server.js
@@ -3,13 +3,23 @@ const routes = require("./routes");
 // import sequelize connection
 const sequelize = require("./config/connection");
 
+require("custom-env").env("staging");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+console.time;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
